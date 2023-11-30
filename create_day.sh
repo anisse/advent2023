f8f5b64d238ebde5b5f7a475db60c803cabea35c
Anisse Astier <anisse@astier.eu>
2023-11-30 22:30:58 +0100

    more quoting

--- create_day.sh
+++ create_day.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 day=$1
 day0=$(printf "%02d" "$1")
 
-if [ ! -f src/bin/day$day0.rs ]; then
-	cp src/bin/template.rs src/bin/day$day0.rs
+if [ ! -f "src/bin/day$day0.rs" ]; then
+	cp src/bin/template.rs "src/bin/day$day0.rs"
 fi
 curl -sSf --output "./src/inputs/day$day0.txt" "https://adventofcode.com/2023/day/$day/input" --compressed -H 'User-Agent: Anisse AoC fetcher' -H 'Referer: https://adventofcode.com/2023/day/$day' -H "Cookie: $(cat cookie.txt)"
