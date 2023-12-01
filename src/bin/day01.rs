c985e8c3793cd40ded85c1f80bc207a86c71fbe5
Anisse Astier <anisse@astier.eu>
2023-12-01 06:55:49 +0100

    day1: cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -17,7 +17,7 @@ where
     I: Iterator<Item = ParsedItem>,
 {
     things
-        .map(|l| l.chars().filter(|c| c.is_digit(10)).collect())
+        .map(|l| l.chars().filter(|c| c.is_ascii_digit()).collect())
         .map(|s: String| {
             [s.chars().next().unwrap(), s.chars().last().unwrap()]
                 .iter()
@@ -32,15 +32,19 @@ where
     I: Iterator<Item = ParsedItem>,
 {
     things
+        /*
         .filter(|s| {
             println!("before: {s}");
             true
         })
+        */
         .map(|s| [first_digit(&s), last_digit(&s)].iter().collect())
+        /*
         .filter(|s| {
             println!("int: {s}");
             true
         })
+        */
         .map(|l: String| l.parse::<usize>().expect("not int"))
         .sum()
 }
@@ -49,18 +53,7 @@ fn first_digit(s: &str) -> char {
     let mut i = 0;
     let mut s = s.to_string();
     while i <= s.len() {
-        s = s[..i]
-            .replace("four", "4")
-            .replace("five", "5")
-            .replace("six", "6")
-            .replace("seven", "7")
-            .replace("nine", "9")
-            .replace("one", "1")
-            .replace("eight", "8")
-            .replace("two", "2")
-            .replace("three", "3")
-            .to_string()
-            + &s[i..];
+        s = replace_digits(&s[..i]) + &s[i..];
         i += 1;
     }
     s.chars().find(|c| c.is_ascii_digit()).unwrap()
@@ -69,23 +62,25 @@ fn last_digit(s: &str) -> char {
     let mut i = s.len();
     let mut s = s.to_string();
     loop {
-        s = s[..i].to_string()
-            + &s[i..]
-                .replace("four", "4")
-                .replace("five", "5")
-                .replace("six", "6")
-                .replace("seven", "7")
-                .replace("nine", "9")
-                .replace("one", "1")
-                .replace("eight", "8")
-                .replace("two", "2")
-                .replace("three", "3");
+        s = s[..i].to_string() + &replace_digits(&s[i..]);
         if i == 0 {
             break;
         }
         i -= 1;
     }
     s.chars().filter(|c| c.is_ascii_digit()).last().unwrap()
+}
+
+fn replace_digits(s: &str) -> String {
+    s.replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
 }
 
 #[test]
