29eb11da4f5d2df7623a4fdca80dd8cb8507694e
Anisse Astier <anisse@astier.eu>
2023-12-03 06:30:02 +0100

    day3: part 1

---
+++ src/bin/day03.rs
@@ -0,0 +1,101 @@
+use advent2023::*;
+fn main() {
+    let things = parse(input!());
+    //part 1
+    let res = operation(things.clone());
+    println!("Part 1: {}", res);
+    //part 2
+    let res = operation2(things);
+    println!("Part 2: {}", res);
+}
+type ParsedItem = Vec<char>;
+fn parse(input: &str) -> impl Iterator<Item = ParsedItem> + Clone + '_ {
+    input.lines().map(|x| x.chars().collect())
+}
+
+fn operation<I>(things: I) -> usize
+where
+    I: Iterator<Item = ParsedItem>,
+{
+    let mut sum = 0;
+    let map: Vec<_> = things.collect();
+    for (y, line) in map.iter().enumerate() {
+        let mut x = 0;
+        while x < line.len() {
+            if let Some((i, l)) = integer(&line[x..]) {
+                if is_adjacent_int(&map, x, y, l) {
+                    sum += i as usize;
+                }
+                x += l;
+            }
+            x += 1;
+        }
+    }
+    sum
+}
+
+fn is_adjacent_int(map: &[Vec<char>], x: usize, y: usize, lx: usize) -> bool {
+    let mut yy: isize;
+    let mut xx: isize;
+    for yy in (y as isize - 1)..=(y as isize + 1) {
+        for xx in (x as isize - 1)..=((x + lx) as isize) {
+            // out of bounds
+            if yy < 0 || xx < 0 || yy as usize >= map.len() || xx as usize >= map[yy as usize].len()
+            {
+                continue;
+            }
+            // skip number
+            if yy == y as isize && ((x as isize)..((x + lx) as isize)).contains(&xx) {
+                continue;
+            }
+            if map[yy as usize][xx as usize] != '.' {
+                println!(
+                    "part {} found at coord ({xx}, {yy}) next to int",
+                    map[yy as usize][xx as usize],
+                );
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn integer(s: &[char]) -> Option<(u16, usize)> {
+    let mut i = 0;
+    while i < s.len() {
+        if s[i].is_ascii_digit() {
+            i += 1
+        } else {
+            break;
+        }
+    }
+    if i == 0 {
+        return None;
+    }
+    dbg!(&s[..i]);
+    return Some((
+        s[..i].iter().collect::<String>().parse().expect("not int"),
+        i,
+    ));
+}
+
+fn operation2<I>(things: I) -> usize
+where
+    I: Iterator<Item = ParsedItem>,
+{
+    for _ in things {
+        todo!()
+    }
+    0
+}
+
+#[test]
+fn test() {
+    let things = parse(sample!());
+    //part 1
+    let res = operation(things.clone());
+    assert_eq!(res, 42);
+    //part 2
+    let res = operation2(things);
+    assert_eq!(res, 42);
+}
