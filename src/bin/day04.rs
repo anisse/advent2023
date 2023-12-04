8738c00280b8ae683a120061d2565a156f3f957b
Anisse Astier <anisse@astier.eu>
2023-12-04 06:31:50 +0100

    day4: remove hardcoded separator and add a test

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,24 +1,33 @@
 use advent2023::*;
 fn main() {
-    let things = parse(input!());
+    let (cards, sep) = parse(input!());
     //part 1
-    let res = operation(things.clone());
+    let res = operation(cards.clone(), sep);
     println!("Part 1: {}", res);
     //part 2
-    let res = operation2(things);
+    let res = operation2(cards, sep);
     println!("Part 2: {}", res);
 }
 type ParsedItem = Vec<u8>;
-fn parse(input: &str) -> impl Iterator<Item = ParsedItem> + Clone + '_ {
-    input.lines().map(|x| {
-        x.split_ascii_whitespace()
-            .skip(2)
-            .filter(|s| *s != "|")
-            .map(|n| n.parse().expect("not int"))
-            .collect()
-    })
+fn parse(input: &str) -> (impl Iterator<Item = ParsedItem> + Clone + '_, usize) {
+    let (sep, _) = input
+        .split_ascii_whitespace()
+        .enumerate()
+        .skip(2)
+        .find(|(_, s)| *s == "|")
+        .expect("a | separator");
+    (
+        input.lines().map(|x| {
+            x.split_ascii_whitespace()
+                .skip(2)
+                .filter(|s| *s != "|")
+                .map(|n| n.parse().expect("not int"))
+                .collect()
+        }),
+        sep - 2, // we did skip the first two fields, so take that into account
+    )
 }
-fn operation<I>(things: I) -> usize
+fn operation<I>(things: I, sep: usize) -> usize
 where
     I: Iterator<Item = ParsedItem>,
 {
@@ -26,10 +35,10 @@ where
     for l in things {
         let mut winning = [false; 100];
         let mut score = 0;
-        for i in l.iter().take(10) {
+        for i in l.iter().take(sep) {
             winning[*i as usize] = true;
         }
-        for n in l.iter().skip(10) {
+        for n in l.iter().skip(sep) {
             if winning[*n as usize] {
                 if score == 0 {
                     score = 1;
@@ -44,7 +53,7 @@ where
     sum
 }
 
-fn operation2<I>(things: I) -> usize
+fn operation2<I>(things: I, sep: usize) -> usize
 where
     I: Iterator<Item = ParsedItem>,
 {
@@ -53,10 +62,10 @@ where
     for (i, l) in cards.iter().enumerate() {
         let mut winning = [false; 100];
         let mut matches = 0;
-        for i in l.iter().take(10) {
+        for i in l.iter().take(sep) {
             winning[*i as usize] = true;
         }
-        for n in l.iter().skip(10) {
+        for n in l.iter().skip(sep) {
             if winning[*n as usize] {
                 matches += 1;
             }
@@ -71,11 +80,11 @@ where
 
 #[test]
 fn test() {
-    let things = parse(sample!());
+    let (cards, sep) = parse(sample!());
     //part 1
-    let res = operation(things.clone());
-    assert_eq!(res, 42);
+    let res = operation(cards.clone(), sep);
+    assert_eq!(res, 13);
     //part 2
-    let res = operation2(things);
-    assert_eq!(res, 42);
+    let res = operation2(cards, sep);
+    assert_eq!(res, 30);
 }
