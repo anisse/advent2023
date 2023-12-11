4229b6e2bcecb8cae3f25a398b1964a551fe3d40
Anisse Astier <anisse@astier.eu>
2023-12-11 06:56:00 +0100

    day11: part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -114,14 +114,64 @@ fn expand_test() {
     assert_eq!(expanded, map2);
 }
 
+fn common(map: MapRef, expand_factor: usize) -> usize {
+    let expand_y: Vec<_> = map
+        .iter()
+        .enumerate()
+        .filter(|(_, l)| l.iter().all(|c| *c == '.'))
+        .map(|(y, _)| y)
+        .collect();
+    let expand_x: Vec<_> = map[0]
+        .iter()
+        .enumerate()
+        .filter(|(x, _)| map.iter().all(|l| l[*x] == '.'))
+        .map(|(x, _)| x)
+        .collect();
+    let coords: Vec<_> = map
+        .iter()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.iter()
+                .enumerate()
+                .filter(|(_, c)| **c == '#')
+                .map(move |(x, _)| (x, y))
+        })
+        .collect();
+
+    let mut sum = 0;
+    for i in 0..coords.len() {
+        for j in (i + 1)..coords.len() {
+            let c1 = coords[i];
+            let c2 = coords[j];
+            let (xmax, xmin, ymax, ymin) = (
+                c1.0.max(c2.0),
+                c1.0.min(c2.0),
+                c1.1.max(c2.1),
+                c1.1.min(c2.1),
+            );
+            let mut ydiff = ymax - ymin;
+            for y in expand_y.iter() {
+                if *y < ymax && *y > ymin {
+                    ydiff += expand_factor - 1;
+                }
+            }
+            let mut xdiff = xmax - xmin;
+            for x in expand_x.iter() {
+                if *x < xmax && *x > xmin {
+                    xdiff += expand_factor - 1;
+                }
+            }
+            sum += ydiff + xdiff;
+        }
+    }
+    sum
+}
 fn part2<I>(things: I) -> usize
 where
     I: Iterator<Item = ParsedItem>,
 {
-    for _ in things {
-        todo!()
-    }
-    42
+    let map: Vec<_> = things.collect();
+    common(&map, 1000000)
 }
 
 #[test]
@@ -131,6 +181,8 @@ fn test() {
     let res = part1(things.clone());
     assert_eq!(res, 374);
     //part 2
-    let res = part2(things);
-    assert_eq!(res, 42);
+    let map: Vec<_> = things.collect();
+    assert_eq!(common(&map, 2), 374);
+    assert_eq!(common(&map, 10), 1030);
+    assert_eq!(common(&map, 100), 8410);
 }
