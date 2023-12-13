19f6349c1b8523e62d5f05b7233fc2347a47819b
Anisse Astier <anisse@astier.eu>
2023-12-13 06:34:39 +0100

    day 13: cleanup and clippy fixes

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -22,14 +22,14 @@ where
 }
 
 fn reflect_score(p: ParsedItem) -> usize {
-    let ROWS = p.len();
-    let COLS = p[0].len();
-    for r in 0..ROWS {
+    let rows = p.len();
+    let cols = p[0].len();
+    for r in 0..rows {
         let mut mirror = true;
         let mut tested = false;
-        'outer: for c in 0..COLS {
+        'outer: for c in 0..cols {
             //println!("Testing row {r}, col {c}");
-            for (r1, r2) in (0..=r).rev().zip((r + 1)..ROWS) {
+            for (r1, r2) in (0..=r).rev().zip((r + 1)..rows) {
                 tested = true;
                 //println!("({r1}, {c}) vs ({r2}, {c}) = {}", p[r1][c] != p[r2][c]);
                 if p[r1][c] != p[r2][c] {
@@ -43,17 +43,17 @@ fn reflect_score(p: ParsedItem) -> usize {
             return (r + 1) * 100;
         }
     }
-    for c in 0..COLS {
+    for c in 0..cols {
         let mut mirror = true;
-        'outer: for r in 0..ROWS {
-            for (c1, c2) in (0..=c).rev().zip((c + 1)..COLS) {
+        (0..rows).for_each(|r| {
+            for (c1, c2) in (0..=c).rev().zip((c + 1)..cols) {
                 //println!("({r}, {c1}) vs ({r}, {c2})");
                 if p[r][c1] != p[r][c2] {
                     mirror = false;
-                    break 'outer;
+                    return;
                 }
             }
-        }
+        });
         if mirror {
             //println!("col {c} is mirror");
             return c + 1;
@@ -68,13 +68,13 @@ where
     things.map(reflect_score_2).sum()
 }
 fn reflect_score_2(p: ParsedItem) -> usize {
-    let ROWS = p.len();
-    let COLS = p[0].len();
-    for r in 0..ROWS {
+    let rows = p.len();
+    let cols = p[0].len();
+    for r in 0..rows {
         let mut unmatch = 0;
-        'outer: for c in 0..COLS {
+        'outer: for c in 0..cols {
             //println!("Testing row {r}, col {c}");
-            for (r1, r2) in (0..=r).rev().zip((r + 1)..ROWS) {
+            for (r1, r2) in (0..=r).rev().zip((r + 1)..rows) {
                 //println!("({r1}, {c}) vs ({r2}, {c}) = {}", p[r1][c] != p[r2][c]);
                 if p[r1][c] != p[r2][c] {
                     unmatch += 1;
@@ -85,25 +85,25 @@ fn reflect_score_2(p: ParsedItem) -> usize {
             }
         }
         if unmatch == 1 {
-            println!("Row {r} is mirror");
+            //println!("Row {r} is mirror");
             return (r + 1) * 100;
         }
     }
-    for c in 0..COLS {
+    for c in 0..cols {
         let mut unmatch = 0;
-        'outer: for r in 0..ROWS {
-            for (c1, c2) in (0..=c).rev().zip((c + 1)..COLS) {
-                println!("({r}, {c1}) vs ({r}, {c2})");
+        (0..rows).for_each(|r| {
+            for (c1, c2) in (0..=c).rev().zip((c + 1)..cols) {
+                //println!("({r}, {c1}) vs ({r}, {c2})");
                 if p[r][c1] != p[r][c2] {
                     unmatch += 1;
                 }
                 if unmatch > 1 {
-                    break 'outer;
+                    return;
                 }
             }
-        }
+        });
         if unmatch == 1 {
-            println!("col {c} is mirror");
+            //println!("col {c} is mirror");
             return c + 1;
         }
     }
