0ae6dc7d4786d3c4912a360330dfd23fb7eafb00
Anisse Astier <anisse@astier.eu>
2023-12-20 11:48:39 +0100

    day 20: not working wip explorations

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -4,8 +4,8 @@ use advent2023::*;
 fn main() {
     let things = parse(input!());
     //part 1
-    let res = part1(&things);
-    println!("Part 1: {}", res);
+    //let res = part1(&things);
+    //println!("Part 1: {}", res);
     //part 2
     let res = part2(&things);
     println!("Part 2: {}", res);
@@ -115,7 +115,7 @@ fn update_state(
             low_count += 1;
         }
         if states.get(&name as &str).is_none() {
-            println!("Reached output {name}");
+            //println!("Reached output {name}");
             continue;
         }
         match states.get_mut(&name as &str).unwrap() {
@@ -137,6 +137,11 @@ fn update_state(
             State::Conjunction(inputs) => {
                 inputs.insert(source.to_string(), high);
                 let send = !inputs.values().all(|i| *i);
+                /*
+                if name == "th" {
+                    println!("Conjunction {name} is sending {send}");
+                }
+                */
                 modules[&name as &str]
                     .outputs
                     .iter()
@@ -164,18 +169,83 @@ fn part2(modules: &ModuleMap) -> usize {
             states.insert(name, State::Conjunction(h));
         }
     });
-    modules.values().for_each(|module| {
+    let mut conjunction_reverse_tree: HashMap<String, Vec<String>> = HashMap::new();
+    modules.iter().for_each(|(name, module)| {
+        if let Conjunction { inputs } = &module.t {
+            conjunction_reverse_tree.insert(name.to_string(), inputs.clone());
+        }
+    });
+    modules.iter().for_each(|(name, module)| {
         for o in &module.outputs {
             if states.get(o).is_none() {
+                assert_eq!(o, &"rx");
                 states.insert(o, State::FlipFlop(false));
+                conjunction_reverse_tree.insert(o.to_string(), vec![name.to_string()]);
+                return;
             }
         }
     });
 
+    conjunction_reverse_tree
+        .iter()
+        .for_each(|(con, inputs)| println!("{con} accepts inputs from {inputs:?}"));
+    #[derive(Debug)]
+    struct ConjHist {
+        count: bool,
+        last_counted: bool,
+        history: Vec<usize>,
+    }
+    let mut conjunction_history: HashMap<String, ConjHist> = HashMap::new();
+    let mut queue = VecDeque::from(["rx".to_string()]);
+    let mut current = false;
+    while let Some(end) = queue.pop_front() {
+        if conjunction_reverse_tree.get(&end).is_none() {
+            continue;
+        }
+        for inp in conjunction_reverse_tree[&end].iter() {
+            conjunction_history.insert(
+                inp.clone(),
+                ConjHist {
+                    count: current,
+                    last_counted: false,
+                    history: vec![],
+                },
+            );
+            queue.push_back(inp.to_string());
+        }
+        current = !current;
+    }
+    dbg!(&conjunction_history);
+
     let mut presses = 0;
     loop {
-        update_state("button", "broadcaster", false, modules, &mut states);
         presses += 1;
+        update_state("button", "broadcaster", false, modules, &mut states);
+        conjunction_history.iter_mut().for_each(|(name, cjh)| {
+            if let State::Conjunction(ins) = &states[&name as &str] {
+                let val = !ins.values().all(|i| *i);
+                if name == "th" {
+                    println!("th ins: {ins:?} out: {val} (expected: {})", cjh.count);
+                }
+                if val == cjh.count {
+                    if cjh.last_counted {
+                        *cjh.history.last_mut().unwrap() += 1;
+                    } else {
+                        cjh.history.push(1);
+                        cjh.last_counted = true;
+                    }
+                } else {
+                    cjh.last_counted = false;
+                }
+                if presses % 1000 == 0 {
+                    dbg!(&cjh.history);
+                }
+                if let Some((start, period)) = cycle_detect(&cjh.history) {
+                    println!("Detected cycle for {name}, starts at {start}, period of {period}");
+                }
+            }
+        });
+
         if let State::FlipFlop(s) = states["rx"] {
             if s {
                 break;
@@ -185,6 +255,44 @@ fn part2(modules: &ModuleMap) -> usize {
         }
     }
     presses
+}
+fn cycle_detect<T>(seq: &[T]) -> Option<(usize, usize)>
+where
+    T: Eq,
+{
+    // basic floyd tortoise and hare implementation
+    let mut tor = 0;
+    let mut har = 0;
+    loop {
+        har += 2;
+        tor += 1;
+        if har >= seq.len() || tor >= seq.len() {
+            return None;
+        }
+        if seq[har] == seq[tor] {
+            break;
+        }
+    }
+    let mut mu = 0;
+    tor = 0;
+    while seq[tor] != seq[har] {
+        tor += 1;
+        har += 1;
+        mu += 1;
+        if har >= seq.len() || tor >= seq.len() {
+            return None;
+        }
+    }
+    let mut lam = 1;
+    har = tor + 1;
+    while seq[tor] != seq[har] {
+        if har >= seq.len() {
+            return None;
+        }
+        har += 1;
+        lam += 1;
+    }
+    Some((mu, lam))
 }
 
 #[test]
