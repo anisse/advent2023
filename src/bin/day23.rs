231507282e8d47c8c0aa79a985e1e704f8ad3485
Anisse Astier <anisse@astier.eu>
2023-12-23 08:03:58 +0100

    day 23: go back to bruteforce solution and optimize it a bit
    Now completes in 4 minutes and a half instead of ~20 minutes

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -68,6 +68,7 @@ fn longest_path(map: MapRef, pos: Pos, end: Pos, current_path: &mut HashSet<Pos>
     max
 }
 
+/*
 fn part2(map: MapRef) -> usize {
     let start_x = map[0].iter().position(|c| *c == b'.').expect("start pos");
     let end_x = map[map.len() - 1]
@@ -92,19 +93,79 @@ fn part2(map: MapRef) -> usize {
 
     child.join().unwrap()
 }
+*/
+fn part2(map: MapRef) -> usize {
+    let start_x = map[0].iter().position(|c| *c == b'.').expect("start pos");
+    let end_x = map[map.len() - 1]
+        .iter()
+        .position(|c| *c == b'.')
+        .expect("end pos");
+
+    let map = map.to_vec();
+    // Needs a bigger stack
+    let child = thread::Builder::new()
+        .stack_size(8 * 1024 * 1024) // this is mostly for debug mode with full input
+        .spawn(move || {
+            longest_path2_bruteforce(
+                &map,
+                (start_x, 0),
+                (end_x, map.len() - 1),
+                &mut vec![vec![false; map[0].len()]; map.len()],
+                0,
+            )
+        })
+        .unwrap();
+
+    child.join().unwrap()
+}
+
+fn longest_path2_bruteforce(
+    map: MapRef,
+    pos: Pos,
+    end: Pos,
+    seen: &mut [Vec<bool>],
+    cur: usize,
+) -> usize {
+    if pos == end {
+        return cur;
+    }
+    let ipos = (pos.0 as isize, pos.1 as isize);
+    let mut max = 0;
+    for d in [(0, 1), (0, -1), (1, 0), (-1, 0)].into_iter() {
+        let inext = (ipos.0 + d.0, ipos.1 + d.1);
+        let next = (inext.0 as usize, inext.1 as usize);
+        if inext.0 < 0 || next.0 >= map[0].len() || inext.1 < 0 || next.1 >= map.len() {
+            continue;
+        }
+        if map[next.1][next.0] == b'#' {
+            continue;
+        }
+        if seen[next.1][next.0] {
+            continue;
+        }
+        seen[next.1][next.0] = true;
+        let len = longest_path2_bruteforce(map, next, end, seen, cur + 1);
+        seen[next.1][next.0] = false;
+        if len > max {
+            max = len;
+        }
+    }
+    max
+}
 
 fn longest_path2(
     map: MapRef,
     pos: Pos,
     end: Pos,
     current_path: &mut HashSet<Pos>,
-    maxes: &mut HashMap<Pos, usize>,
+    maxes: &mut HashMap<Pos, (usize, usize)>,
 ) -> usize {
+    println!("At pos {pos:?}, current length is {}", current_path.len());
+    _print_map(map, current_path);
     if pos == end {
         return current_path.len();
     }
     let ipos = (pos.0 as isize, pos.1 as isize);
-    let mut max_current = 0;
     for d in [(0, 1), (0, -1), (1, 0), (-1, 0)].into_iter() {
         let inext = (ipos.0 + d.0, ipos.1 + d.1);
         let next = (inext.0 as usize, inext.1 as usize);
@@ -117,24 +178,31 @@ fn longest_path2(
         if current_path.contains(&next) {
             continue;
         }
-        let max = *maxes.entry(next).or_insert(0);
-        if max > current_path.len() {
+        let max = *maxes.entry(next).or_insert((0, current_path.len()));
+        if max.1 > current_path.len() && max.0 > 0 {
             continue;
         }
         current_path.insert(next);
         let len = longest_path2(map, next, end, current_path, maxes);
         current_path.remove(&next);
-        if len > max {
-            maxes.insert(next, len);
-        }
-        if len > max_current {
-            max_current = len;
+        if len > max.0 {
+            maxes.insert(pos, (len, current_path.len()));
         }
     }
-    if *maxes.entry(pos).or_insert(0) < max_current {
-        maxes.insert(pos, max_current);
-    }
-    maxes[&pos]
+    *maxes.values().map(|(m, _)| m).max().expect("a max")
+}
+
+fn _print_map(map: MapRef, current_path: &mut HashSet<Pos>) {
+    map.iter().enumerate().for_each(|(y, l)| {
+        l.iter().enumerate().for_each(|(x, c)| {
+            if current_path.contains(&(x, y)) {
+                print!("O")
+            } else {
+                print!("{}", *c as char)
+            }
+        });
+        println!();
+    });
 }
 
 #[test]
