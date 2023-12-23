cf0accdb316e8f98c26706a56d44569960380c2b
Anisse Astier <anisse@astier.eu>
2023-12-23 06:27:34 +0100

    day 23: part 2 bruteforce

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -66,10 +66,45 @@ fn longest_path(map: MapRef, pos: Pos, end: Pos, current_path: &mut HashSet<Pos>
 }
 
 fn part2(map: MapRef) -> usize {
-    for _ in map.iter() {
-        todo!()
+    let start_x = map[0].iter().position(|c| *c == b'.').expect("start pos");
+    let end_x = map[map.len() - 1]
+        .iter()
+        .position(|c| *c == b'.')
+        .expect("end pos");
+    longest_path2(
+        map,
+        (start_x, 0),
+        (end_x, map.len() - 1),
+        &mut HashSet::new(),
+    )
+}
+
+fn longest_path2(map: MapRef, pos: Pos, end: Pos, current_path: &mut HashSet<Pos>) -> usize {
+    if pos == end {
+        return current_path.len();
     }
-    42
+    let ipos = (pos.0 as isize, pos.1 as isize);
+    let mut max = 0;
+    for d in [(0, 1), (0, -1), (1, 0), (-1, 0)].into_iter() {
+        let inext = (ipos.0 + d.0, ipos.1 + d.1);
+        let next = (inext.0 as usize, inext.1 as usize);
+        if inext.0 < 0 || next.0 >= map[0].len() || inext.1 < 0 || next.1 >= map.len() {
+            continue;
+        }
+        if map[next.1][next.0] == b'#' {
+            continue;
+        }
+        if current_path.contains(&next) {
+            continue;
+        }
+        current_path.insert(next);
+        let len = longest_path2(map, next, end, current_path);
+        current_path.remove(&next);
+        if len > max {
+            max = len;
+        }
+    }
+    max
 }
 
 #[test]
@@ -77,8 +112,8 @@ fn test() {
     let map = parse(sample!());
     //part 1
     let res = part1(&map);
-    assert_eq!(res, 42);
+    assert_eq!(res, 94);
     //part 2
-    //let res = part2(&map);
-    //assert_eq!(res, 42);
+    let res = part2(&map);
+    assert_eq!(res, 154);
 }
